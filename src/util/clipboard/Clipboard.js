b58a1ed2bb3b6f78f6608e9594a107baf846e5a4
hello_chenchen <welcomechenchen@gmail.com>
2019-01-01 20:52:12 +0800

    try to use nodobjc and objc, but failed

--- src/util/clipboard/Clipboard.js
+++ src/util/clipboard/Clipboard.js
@@ -1,9 +1,12 @@
 const {clipboard} = require('electron');
+var $ = require('nodobjc');
 
 class Clipboard {
   static readBuffer() {
     let availableFormats = clipboard.read('CBF_TEXT');
-    console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);  
+    // console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);
+    let demo = "ccccc";
+    // console.log($.NSString("stringWithUTF8String", demo));
     return availableFormats;
   }
 }
