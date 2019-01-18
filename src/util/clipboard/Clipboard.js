7f6977cb4291be59698f82e2ab6cb5e1ac3d05c0
hello_chenchen <welcomechenchen@gmail.com>
2019-01-18 19:04:32 +0800

    sqlite3 test

--- src/util/clipboard/Clipboard.js
+++ src/util/clipboard/Clipboard.js
@@ -7,24 +7,26 @@ class Clipboard {
     let availableFormats = clipboard.read('CBF_TEXT');
     // console.log("hello_chenchen: " + binding.hello());
     // console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);  
-    console.log(clipboard);
     console.log(Database);
-    // var db = db1.Database(':memory:');
-    // db.serialize(function() {
-    //   db.run("CREATE TABLE lorem (info TEXT)");
+
+    var db = new Database(':memory:');
+
+    db.serialize(function() {
+      db.run("CREATE TABLE lorem (info TEXT)");
     
-    //   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    //   for (var i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    //   }
-    //   stmt.finalize();
+      var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
     
-    //   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    //     console.log(row.id + ": " + row.info);
-    //   });
-    // });
+      db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+        console.log(row.id + ": " + row.info);
+      });
+    });
     
-    // db.close();
+    db.close();
+
     return availableFormats;
   }
 }
