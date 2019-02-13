6d6588cde8b0318a439aae57a5111aa9d797f973
hello_chenchen <welcomechenchen@gmail.com>
2019-02-13 20:13:40 +0800

    function-pre: use clipboard from my github repository and try to call foo function, but not work

--- src/util/clipboard/Clipboard.js
+++ src/util/clipboard/Clipboard.js
@@ -1,14 +1,15 @@
-import { clipboard } from 'electron';
+// import { clipboard } from 'electron';
 // import { Screen } from 'robotjs';
 // import { Database } from 'sqlite3';
 var Database = require('sqlite3');
 var Screen = require('robotjs');
+var clipboard11 = require('clipboard');
 
 class Clipboard {
 
   constructor() {}
   readBuffer() {
-    let availableFormats = clipboard.read('CBF_TEXT');
+    // let availableFormats = clipboard.read('CBF_TEXT');
     // console.log("hello_chenchen: " + binding.hello());
     // console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);
 
@@ -17,7 +18,10 @@ class Clipboard {
     var screenSize = Screen.getScreenSize();
     console.log(JSON.stringify(screenSize));
 
-    return availableFormats;
+    var n = clipboard11.foo();
+    console.log(n);
+
+    return "ccc";
   }
 }
 
