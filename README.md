dbddfc83e987f0cbda6d3d0186f05f1b91837494
hello_chenchen <welcomechenchen@gmail.com>
2019-02-15 21:29:52 +0800

    function-alpha: can use macos system apigit add ./!  yes,23333

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ rebuild for **all platforms**:
 
 `npm rebuild --runtime=electron --target=4.0.1 --disturl=https://atom.io/download/atom-shell --abi=64`
 
+## install clipboard for electron:
+for **mac**:
+
+`npm install clipboard git+https://github.com/hello-chenchen/node-clipboard-api-adapter.git --build-from-source --runtime=electron --target=4.0.1`
 
 ### FIXED:
 Q:`error MSB8020: The build tools for v140 (Platform Toolset = 'v140') cannot be found. To build using the v140 build tools, please install v140 build tools.  Alternatively, you may upgrade to the current Visual Studio tools by selecting the Project menu or right-click the solution, and then selecting "Retarget solution".`
--- src/util/clipboard/Clipboard.js
+++ src/util/clipboard/Clipboard.js
@@ -2,7 +2,7 @@
 // import { Screen } from 'robotjs';
 // import { Database } from 'sqlite3';
 var Database = require('sqlite3');
-var Screen = require('robotjs');
+// var Screen = require('robotjs');
 var clipboard11 = require('clipboard');
 
 class Clipboard {
@@ -14,9 +14,9 @@ class Clipboard {
     // console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);
 
     console.log(Database);
-    console.log(Screen);
-    var screenSize = Screen.getScreenSize();
-    console.log(JSON.stringify(screenSize));
+    // console.log(Screen);
+    // var screenSize = Screen.getScreenSize();
+    // console.log(JSON.stringify(screenSize));
 
     var n = clipboard11.foo();
     console.log(n);
