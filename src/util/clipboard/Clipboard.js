bc3b591b81c2ddefdbf9d0f9ff0026062258b7f7
hello-chenchen <welcomechenchen@gmail.com>
2019-02-24 02:07:47 +0800

    feature-pre: support clipboard linux, but not work

--- src/util/clipboard/Clipboard.js
+++ src/util/clipboard/Clipboard.js
@@ -1,24 +1,11 @@
-// import { clipboard } from 'electron';
-// import { Screen } from 'robotjs';
-// import { Database } from 'sqlite3';
-var Database = require('sqlite3');
-// var Screen = require('robotjs');
-var clipboard11 = require('clipboard');
+var clipboard = require('clipboard');
 
 class Clipboard {
 
   constructor() {}
   readBuffer() {
-    // let availableFormats = clipboard.read('CBF_TEXT');
-    // console.log("hello_chenchen: " + binding.hello());
-    // console.log("hello_chenchen: " + JSON.stringify(availableFormats) + "length: " + availableFormats.length);
 
-    console.log(Database);
-    // console.log(Screen);
-    // var screenSize = Screen.getScreenSize();
-    // console.log(JSON.stringify(screenSize));
-
-    var n = clipboard11.foo();
+    var n = clipboard.foo();
     console.log(n);
 
     return "ccc";
